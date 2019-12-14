42011c0183a2c94d95c599e835ba68bc60d425f9
Rogelio Maldonado <rogermz.projects@gmail.com>
2019-12-14 12:47:59 -0600

    Prueba de conexion externar a mysql (docker)

--- app/db.php
+++ app/db.php
@@ -4,10 +4,13 @@ function OpenConnection()
 {
   session_write_close();
   $serverName = "138.68.2.24"; //serverName\instanceName
+  $port = 3308;
   $Database = "tg2019_almacen";
-  $Uid = "tgurus_alumno";
-  $PWD = "tg2019";
-  $conn = new mysqli($serverName, $Uid, $PWD, $Database);
+  //$Uid = "tgurus_alumno";
+  //$PWD = "tg2019";
+  $Uid = "root";
+  $PWD = "devtg";
+  $conn = new mysqli($serverName, $Uid, $PWD, $Database, $port);
   $conn->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
   $conn->set_charset("utf8");
   if( $conn ) {
